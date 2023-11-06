6128c19c833f66ddd3f8f02698d2345e23c88656
meesam4687 <meesam4687@gmail.com>
2023-11-06 20:05:04 +0530

    Fetch Forecast

--- util/fetch.py
+++ util/fetch.py
@@ -12,3 +12,14 @@ def weather(location):
             print(f"Request failed : {response.status_code}")
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
+def forecast(location):
+    url = f"https://api.weatherapi.com/v1/forecast.json?q={location.lower()}&key=5d7c3c1717bd4642979153713230206&t={t}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            print(f"Request failed : {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
